ee6c96c13d919239a6346b3dc4a22e07b1b81397
Foysal23546 <174034854+Foysal23546@users.noreply.github.com>
2026-04-24 20:45:21 +0600

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -14,5 +14,14 @@ int main(){
     }
     cout << min << endl;
     cout << max << endl;
+    // int size = 10,minn = 0,maxx = 0;
+    // int arr[size] = {52,42,65,62,74,23,85,34,77,67};
+    // minn = arr[0],maxx = arr[0];
+    // for(int i = 0; i<size;i++){
+    //     minn = min(arr[i],minn);
+    //     maxx = max(arr[i],maxx);
+    // }
+    // cout << minn << endl;
+    // cout << maxx << endl;
     return 0;
 }
